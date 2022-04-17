a952f26874dea9becb13f3514517f8232382a390
Winston Smith <wsbraves@gmail.com>
2022-04-17 12:26:22 -0400

    Initialize repository.

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Logo from './Logo.svelte'
+</script>
+
+<main>
+  <!-- <h1>wordfinder</h1> -->
+  <Logo />
+  <input type="search" />
+</main>
+
+<style>
+</style>
---
+++ src/Logo.svelte
@@ -0,0 +1,34 @@
+ <svg
+    id="Logo_1"
+    data-name="Logo 1"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 482.08 871.57"
+    height="100px"
+    ><defs
+      ><style>
+        .cls-1 {
+          fill: #414042;
+          stroke: #231f20;
+          stroke-miterlimit: 10;
+          stroke-width: 20px;
+        }
+        .cls-2 {
+          font-size: 599.49px;
+        }
+        .cls-2,
+        .cls-3 {
+          fill: #fff;
+          font-family: Gabriola;
+        }
+        .cls-3 {
+          font-size: 432.88px;
+          letter-spacing: 0.01em;
+        }
+      </style></defs
+    ><circle class="cls-1" cx="241.04" cy="395.52" r="231.04" /><text
+      class="cls-2"
+      transform="translate(37.8 467.03) scale(0.94 1)">w</text
+    ><text class="cls-3" transform="translate(301.44 529.06) scale(0.95 1)"
+      >f</text
+    ></svg
+  >
